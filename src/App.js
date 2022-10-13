71b942133808d8b67b7f1c9d9bd6f12bd0642c28
DanielEmidio1988 <daniel.emidio.silva@gmail.com>
2022-10-13 15:57:37 -0300

    atualização props

--- src/App.js
+++ src/App.js
@@ -5,6 +5,63 @@ import "./styles.css";
 
 export default function App() {
 
+  const video1 = {
+    imagem: "https://picsum.photos/400/400?a=1",
+    titulo: "TESTE",
+    nome: "Lucas",
+    imagemUsuario: "https://media-exp1.licdn.com/dms/image/D4D03AQGZLsmVQh1-Jw/profile-displayphoto-shrink_400_400/0/1664313008886?e=1671062400&v=beta&t=bYaqG4AbPQJcj3eLWrgji_98RYUdilJKj8iqvKTRKFc"
+  }
+
+  const video2 = {
+    imagem: "https://picsum.photos/400/400?a=2",
+    titulo: "TituloDiferente",
+    nome: "Daniel",
+    imagemUsuario: "https://media-exp1.licdn.com/dms/image/D4D03AQGZLsmVQh1-Jw/profile-displayphoto-shrink_400_400/0/1664313008886?e=1671062400&v=beta&t=bYaqG4AbPQJcj3eLWrgji_98RYUdilJKj8iqvKTRKFc"
+  }
+
+  const video3 = {
+    imagem: "https://picsum.photos/400/400?a=3",
+    titulo: "TituloMaisDiferente",
+    nome: "Chiquinho",
+    imagemUsuario: "https://media-exp1.licdn.com/dms/image/D4D03AQGZLsmVQh1-Jw/profile-displayphoto-shrink_400_400/0/1664313008886?e=1671062400&v=beta&t=bYaqG4AbPQJcj3eLWrgji_98RYUdilJKj8iqvKTRKFc"
+  }
+
+  const video4 = {
+    imagem: "https://picsum.photos/400/400?a=4",
+    titulo: "TituloMaisDiferente",
+    nome: "Zezinho",
+    imagemUsuario: "https://media-exp1.licdn.com/dms/image/D4D03AQGZLsmVQh1-Jw/profile-displayphoto-shrink_400_400/0/1664313008886?e=1671062400&v=beta&t=bYaqG4AbPQJcj3eLWrgji_98RYUdilJKj8iqvKTRKFc"
+  }
+
+  const video5 = {
+    imagem: "https://picsum.photos/400/400?a=5",
+    titulo: "TituloMaisDiferente",
+    nome: "Andre",
+    imagemUsuario: "https://media-exp1.licdn.com/dms/image/D4D03AQGZLsmVQh1-Jw/profile-displayphoto-shrink_400_400/0/1664313008886?e=1671062400&v=beta&t=bYaqG4AbPQJcj3eLWrgji_98RYUdilJKj8iqvKTRKFc"
+  }
+
+  const video6 = {
+    imagem: "https://picsum.photos/400/400?a=6",
+    titulo: "TituloMaisDiferente",
+    nome: "Luizinho",
+    imagemUsuario: "https://media-exp1.licdn.com/dms/image/D4D03AQGZLsmVQh1-Jw/profile-displayphoto-shrink_400_400/0/1664313008886?e=1671062400&v=beta&t=bYaqG4AbPQJcj3eLWrgji_98RYUdilJKj8iqvKTRKFc"
+  }
+
+  const video7 = {
+    imagem: "https://picsum.photos/400/400?a=7",
+    titulo: "TituloMaisDiferente",
+    nome: "Marcos",
+    imagemUsuario: "https://media-exp1.licdn.com/dms/image/D4D03AQGZLsmVQh1-Jw/profile-displayphoto-shrink_400_400/0/1664313008886?e=1671062400&v=beta&t=bYaqG4AbPQJcj3eLWrgji_98RYUdilJKj8iqvKTRKFc"
+  }
+
+  const video8 = {
+    imagem: "https://picsum.photos/400/400?a=8",
+    titulo: "TituloMaisDiferente",
+    nome: "João",
+    imagemUsuario: "https://media-exp1.licdn.com/dms/image/D4D03AQGZLsmVQh1-Jw/profile-displayphoto-shrink_400_400/0/1664313008886?e=1671062400&v=beta&t=bYaqG4AbPQJcj3eLWrgji_98RYUdilJKj8iqvKTRKFc"
+  }
+
+
   return (
     <div>
       <div className="tela-inteira">
@@ -27,14 +84,30 @@ export default function App() {
 
           <section className="painel-de-videos">
 
-          <CardVideo/>
-          <CardVideo/>
-          <CardVideo/>
-          <CardVideo/>
-          <CardVideo/>
-          <CardVideo/>
-          <CardVideo/>
-          <CardVideo/>
+          <CardVideo
+          video={video1}/>
+                    
+          <CardVideo
+          video={video2}/>
+                    
+          <CardVideo
+          video={video3}/>
+
+          <CardVideo
+          video={video4}/>
+
+          <CardVideo
+          video={video5}/>
+
+          <CardVideo
+          video={video6}/>
+
+          <CardVideo
+          video={video7}/>
+
+          <CardVideo
+          video={video8}/>
+
 
           </section>
 
